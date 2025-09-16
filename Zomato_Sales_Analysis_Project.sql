6c03ade60bbcbe30a6e3ccb3e7f4be9eb693f989
Sairaj Soma <sairajsoma1@gmail.com>
2025-09-16 13:07:20 +0530

    Add files via upload

--- Zomato_Sales_Analysis_Project.sql
+++ Zomato_Sales_Analysis_Project.sql
@@ -4,7 +4,71 @@
 --3)sales table
 
 
--- 1) What is the total amount each customer spent on zomato
+--Creating the gold users table who are the premium members and inserting the values:
+
+CREATE TABLE goldusers_signup(userid integer,gold_signup_date date); 
+
+INSERT INTO goldusers_signup(userid,gold_signup_date) 
+ VALUES (1,'09-22-2017'),
+(3,'04-21-2017');
+
+--Creating the users table and inserting the values:
+
+CREATE TABLE users(userid integer,signup_date date); 
+
+INSERT INTO users(userid,signup_date) 
+ VALUES (1,'09-02-2014'),
+(2,'01-15-2015'),
+(3,'04-11-2014');
+
+--Creating the sales table and inserting the values:
+
+CREATE TABLE sales(userid integer,created_date date,product_id integer); 
+
+INSERT INTO sales(userid,created_date,product_id) 
+ VALUES (1,'04-19-2017',2),
+(3,'12-18-2019',1),
+(2,'07-20-2020',3),
+(1,'10-23-2019',2),
+(1,'03-19-2018',3),
+(3,'12-20-2016',2),
+(1,'11-09-2016',1),
+(1,'05-20-2016',3),
+(2,'09-24-2017',1),
+(1,'03-11-2017',2),
+(1,'03-11-2016',1),
+(3,'11-10-2016',1),
+(3,'12-07-2017',2),
+(3,'12-15-2016',2),
+(2,'11-08-2017',2),
+(2,'09-10-2018',3);
+
+--Creating the Product table and inserting the values in a table:
+
+CREATE TABLE product(product_id integer,product_name text,price integer); 
+
+INSERT INTO product(product_id,product_name,price) 
+ VALUES
+(1,'p1',980),
+(2,'p2',870),
+(3,'p3',330);
+
+
+
+
+
+
+
+
+
+
+
+
+
+--Applying  Queries to the given tables to get the important business insights.
+
+
+-- 1) What is the total amount each customer spent on zomato?
 
 select a.UserID, sum(b.price) as total_amt_spent
 from sales a inner join Product b on a.product_id=b.product_id group by a.UserID
